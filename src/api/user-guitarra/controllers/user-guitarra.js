bbac60e9d310148e4aa7a45f439b52aee10f496b
Alef Silveira <alefo28@gmail.com>
2024-09-19 19:17:50 -0600

    first commit

---
+++ src/api/user-guitarra/controllers/user-guitarra.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-guitarra controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-guitarra.user-guitarra');
---
+++ src/api/user-guitarra/routes/user-guitarra.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-guitarra router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-guitarra.user-guitarra');
